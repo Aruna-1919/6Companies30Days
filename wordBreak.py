21f2fe7e857a493fa453eaf5c11c16f7f147185d
Aruna Bharathi Jadam <116643536+Aruna-1919@users.noreply.github.com>
2024-01-15 22:53:42 +0530

    Create wordBreak.py
    Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
    
    Note that the same word in the dictionary may be reused multiple times in the segmentation.

---
+++ wordBreak.py
@@ -0,0 +1,8 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        words = set(wordDict)
+        n = len(s)
+        f = [True] + [False] * n
+        for i in range(1, n + 1):
+            f[i] = any(f[j] and s[j:i] in words for j in range(i))
+        return f[n]
