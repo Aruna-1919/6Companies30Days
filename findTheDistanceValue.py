94e44e3a89cd25f057110f29a91eb3f1d899c0de
Aruna Bharathi Jadam <116643536+Aruna-1919@users.noreply.github.com>
2024-01-21 23:38:31 +0530

    Update findTheDistanceValue.py

--- findTheDistanceValue.py
+++ findTheDistanceValue.py
@@ -1,1 +1,9 @@
-all([abs(i-arr2[j])>d for j in range(len(arr2))])
+class Solution:
+    def findTheDistanceValue(self, arr1: List[int], arr2: List[int], d: int) -> int:
+        c=0
+        for i in arr1:
+            if all([abs(i-arr2[j])>d for j in range(len(arr2))]):
+                c=c+1
+        print(c)
+        return c
+        
